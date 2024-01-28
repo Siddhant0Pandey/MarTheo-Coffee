0fbe2684655cd5a4bd1feee5d2d735aae1a787aa
Siddhant0Pandey <pandeysiddhant0922@gmail.com>
2024-01-28 20:22:56 +0545

    Product comparsion added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import HeroSection from "./component/HeroSection";
 import NavSection from "./component/NavSection";
+import ProductComparison from "./component/ProductComparison";
 
 function App() {
   return (
     <div>
       <NavSection />
       <HeroSection />
+      <ProductComparison />
     </div>
   );
 }
---
+++ src/component/ProductComparison.jsx
@@ -0,0 +1,58 @@
+function ProductComparison() {
+  return (
+    <div className="w-[80vw] h-[90vh] mx-auto mt-[3vh]">
+      <div className="flex gap-10">
+        <div className="card px-8 bg-primary pt-8 pb-[8rem]">
+          <h1 className="font-bold text-2xl mb-6">Prouduct One</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident,
+            nam.
+          </p>
+          <button className="p-4 bg-primary border text-white mt-6">
+            Learn More
+          </button>
+          <hr className="mt-8" />
+          <h3 className="list-none mt-6">
+            <li className="">$10 Minimum Requirement</li>
+            <li>No balance required</li>
+            <li>balance required</li>
+          </h3>
+        </div>
+        <div className="card p-10">
+          <h1>Prouduct One</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident,
+            nam.
+          </p>
+          <button className="p-4 bg-primary border text-white mt-6">
+            Learn More
+          </button>
+          <hr className="mt-8" />
+          <h3 className="list-none mt-6">
+            <li className="">$10 Minimum Requirement</li>
+            <li>No balance required</li>
+            <li>balance required</li>
+          </h3>
+        </div>
+        <div className="card p-10">
+          <h1>Prouduct One</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident,
+            nam.
+          </p>
+          <button className="p-4 bg-primary border text-white mt-6">
+            Learn More
+          </button>
+          <hr className="mt-8" />
+          <h3 className="list-none mt-6">
+            <li className="">$10 Minimum Requirement</li>
+            <li>No balance required</li>
+            <li>balance required</li>
+          </h3>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProductComparison;
